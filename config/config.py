d80be0ec96d19560894432245c5257b7d81f4d9c
joevaas <133934323+joevaas@users.noreply.github.com>
2025-03-10 13:32:26 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -168,7 +168,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", None)
+START_IMG_URL = getenv("START_IMG_URL", https://ibb.co/NdBVRr3V)
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
